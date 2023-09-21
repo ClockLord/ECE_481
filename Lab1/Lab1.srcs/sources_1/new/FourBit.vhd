0e9044b6e9e03e3ed2351627882b6451b6930e38
Nathan Turner <ndturner@mail.bradley.edu>
2023-09-20 20:35:25 -0500

    replaced cout port with internal signal

--- Lab1/Lab1.srcs/sources_1/new/FourBit.vhd
+++ Lab1/Lab1.srcs/sources_1/new/FourBit.vhd
@@ -1,43 +1,14 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 09/20/2023 07:28:52 PM
--- Design Name: 
--- Module Name: FourBit - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity FourBit is
     port (
         A : in std_logic_vector(3 downto 0);
         B : in std_logic_vector(3 downto 0);
         Sum : out std_logic_vector(3 downto 0);
-        Cout : inout std_logic_vector(3 downto 0);
         Op_Sel : in std_logic
     );
 end FourBit;
@@ -56,7 +27,7 @@ architecture Behavioral of FourBit is
     end component;
     
     signal Bor : std_logic_vector(3 downto 0);
-    
+    signal Cout :std_logic_vector(3 downto 0);
     
 begin
 
