f29c7776014d5fb8731e2d50bc80553d3d5a7b11
Nathan Turner <ndturner@mail.bradley.edu>
2023-10-12 11:08:58 -0500

    added comments with code structure.

--- Lab3/Lab3.srcs/sources_1/new/encrypt_design.vhd
+++ Lab3/Lab3.srcs/sources_1/new/encrypt_design.vhd
@@ -22,24 +22,37 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-
+--port description
 entity encrypt_design is
 
         Port (  
         clk : IN STD_LOGIC;
         load_key : IN STD_LOGIC;
-        LFSR_out : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+        key_out : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
         input_val : IN STD_LOGIC_VECTOR (7 DOWNTO 0);
         encrypt_cntl : IN STD_LOGIC;
         output_val : OUT STD_LOGIC_VECTOR (7 DOWNTO 0)
-           );
+        );
            
 end encrypt_design;
 
 architecture Behavioral of encrypt_design is
 
+--we need to store the initial values of the LFSR
+signal LFSR_init : std_logic_vector(7 downto 0) := "00110100"; --value correcponds to 34 in hex
+
 --need to make state machine
 begin
-if rising_edge 
 
+--this will push the state machine forwards
+--so this shifting needs to have a predetermined number of shifts
+    process(clk)
+    begin
+        --this is where the "feedback polynomial" takes place.
+        if rising_edge(clk) then
+        
+        
+        end if;
+    end process;
+    
 end Behavioral;
